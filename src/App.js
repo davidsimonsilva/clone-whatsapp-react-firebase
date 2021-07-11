facea43dc49e39d4252878056b2d7993e906fc34
David Simon <59507155+davidsimonsilva@users.noreply.github.com>
2021-07-11 15:02:52 -0300

    Ícone e buscador
    Adição de um ícone de teste e uma barra de busca para pesquisar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import './components/ChatListItem';
 
-function App() {
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+export default function app ({app}) {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-window">
+      <div className="sidebar">
+
+        <header>
+          <img className="header--avatar" src="https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659652_960_720.png" alt="UserIcon" />
+          <div className="header--buttons">
+
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}} />
+            </div>
+            
+          </div>
+        </header>
+
+      <div clasName="search">
+        <div className="search--input">
+          <SearchIcon fontSize="small" style={{color: '#919191'}} />
+          <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+        </div>
+      </div>
+
+      <div className="chatlist">
+
+      </div>
+
+      </div>
+      <div className="contentarea">
+
+      </div>
     </div>
   );
-}
-
-export default App;
+};
